e93deaf0cf79705fcf792a7ed736e7739e736ef6
enkelena <enkelena.h@gmail.com>
2018-06-14 18:12:12 +0200

    Add files via upload

---
+++ notification/notification.js
@@ -0,0 +1,8 @@
+
+ browser.notifications.create({
+        "type":"basic",
+        "iconUrl":"f.jpg",
+        "title": "Free software habits",
+        "show": "alert(window.location.origin)"
+
+    });
