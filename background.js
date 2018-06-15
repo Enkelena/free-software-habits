210025bfb6d1432fb43a44be6fa9abdb36776975
enkelena <enkelena.h@gmail.com>
2018-06-15 17:49:08 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,21 @@
+// add listener to browser tab update
+browser.tabs.onUpdated.addListener(tabId => {
+	initialize(tabId);
+});
+
+function initialize(tabId) {
+
+		browser.browserAction.setIcon({
+		tabId: tabId,
+		path: "normalView/icons/f.jpg"
+	})
+
+	browser.browserAction.setTitle({
+		tabId: tabId,
+		title: "Free software habits"
+	})
+
+
+	browser.browserAction.show(tabId);
+	
+}
