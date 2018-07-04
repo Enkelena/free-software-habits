6b8ddf576d58315ee74940283c4536a243e55d1e
Enkelena Haxhiu <enkelena@mylenovo1.example.org>
2018-07-04 18:04:27 +0200

    changes on notification.js

--- notification/notification.js
+++ notification/notification.js
@@ -1,7 +1,7 @@
 ﻿﻿function updateCurrentUrl() {
     function logTabs(tabs) {
-        if(tabs[0].url) {
-            handleMessage(tab[0].url);
+        if(tab[0].url) {
+            handleMessage(tabs[0].url);
         }
      }
     function onError(err){
@@ -47,143 +47,11 @@ function showNotification(currentURL, betterAlternative) {
     const alternativeDomain = alternativeURL.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im)[1];
 
     const message = `You are on: ${currentDomain} for better alternative use: ${alternativeDomain}`;
+
     browser.notifications.create({
         "type":"basic",
         "iconUrl":"f.svg",
         "title": "Free software habits",
         "message": message
-    });
+    }); 
 }
-
-
-
-const alternativeApps = {
-
-    "softwaresList":[
- 
-        
-       {
-          "name":"hangouts",
-          "url":[
-             "https://hangouts.google.com/"
-          ],
-          "alternatives":[
-             {
-                "name":"Jitsi Meet",
-                "url":"https://jitsi.org/jitsi-meet/"
-             }
-          ]
-       },
- 
-       {
-        "name":"Gmail",
-        "url":[
-           "https://mail.google.com/mail/u/0/#inbox"
-        ],
-        "alternatives":[
-           {
-              "name":"thunderbird",
-              "url":"https://www.thunderbird.net/en-US/"
-           }
-        ]
-     },
- 
-       {
-          "name":"freedcamp",
-          "url":[
-            "https://freedcamp.com/"
-            
-          ],
-            
-          "alternatives":[
-             {
-                "name":"Kanboard",
-                "url":"https://kanboard.org/"
-             }
-          ]
-       },
-
-       {
-        "name":"github",
-        "url":[
-          "https://github.com/"
-        ],
-        "alternatives":[
-           {
-              "name":"salsa.debian",
-              "url":"https://salsa.debian.org/"
-           }
-        ]
-     },
-
-     {
-        "name":"trello",
-        "url": [
-            "https://trello.com/"       
-        ],
-        "alternatives":[
-           {
-              "name":"Kanboard",
-              "url":"https://kanboard.org/"
-           }
-        ]
-     },
-
-     {
-        "name":"skype",
-        "url": [
-            "https://www.skype.com/en/"       
-        ],
-        "alternatives":[
-           {
-              "name":"jitsi-meet",
-              "url":"https://jitsi.org/jitsi-meet/"
-           }
-        ]
-     },
-
-
-     {
-        "name":"google-drive",
-        "url": [
-            "https://www.google.com/drive/"       
-        ],
-        "alternatives":[
-           {
-              "name":"seafile",
-              "url":"https://www.seafile.com/en/home/"
-           }
-        ]
-     },
-
-
-     {
-        "name":"dropbox",
-        "url": [
-            "https://www.dropbox.com/"       
-        ],
-        "alternatives":[
-           {
-              "name":"seafile",
-              "url":"https://www.seafile.com/en/home/"
-           }
-        ]
-     },
-
-
-     {
-        "name":"photoshop",
-        "url": [
-            "https://www.photoshop.com/"       
-        ],
-        "alternatives":[
-           {
-              "name":"gimp",
-              "url":"https://www.gimp.org/"
-           }
-        ]
-     }
-
- 
-    ]
- }
