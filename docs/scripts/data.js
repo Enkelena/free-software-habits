eed55ecbcd1a7c607be32e01c136270960a6c992
Bruno Magalhaes <bfmags@gmail.com>
2018-08-11 17:49:52 +0100

    review, tweak style, refactor search

--- docs/scripts/data.js
+++ docs/scripts/data.js
@@ -1,9 +1,8 @@
 
 function generateApp(softwareList) {
-    html = ""
+    let html = "";
     softwareList['softwareList'].forEach(element => {
-
-        html += "<tr>";
+        html += "<tr><td></td>";
         html += "<td><a href='" + element['url'][0] + "'>" + element['name'] + "</a></td>";
         html += "<td>"
         element['alternatives'].forEach(alternative => {
@@ -19,4 +18,34 @@ function generateApp(softwareList) {
 //gets software list from server (github)
 fetch('https://raw.githubusercontent.com/Enkelena/free-software-habits/master/alternativeApps.json')
   .then( response => response.json() )
-  .then( softwareList => generateApp(softwareList));
+  .then( softwareList => {
+        generateApp(softwareList);
+        //search bar
+        const searchBar = document.getElementById('search-bar');
+        searchBar.onkeyup = (el) => {
+            const searchText = el.target.value;
+            if (el.target.value.replace(/\s/g, '').length >= 1){
+                const filteredSoftwareList = [];
+                filteredSoftwareList['softwareList'] = softwareList['softwareList'].filter ( entry => {
+                        return entry.name.toLowerCase().search(searchText.toLowerCase()) > -1;
+                    });
+                generateApp(filteredSoftwareList);
+            } else {
+                generateApp(softwareList);
+            }
+        }
+    });
+
+//show about box
+const show_about_link = document.getElementById('info-link');
+show_about_link.onclick = () => {
+    window.scrollTo(0,0);
+    document.body.style.overflow = 'hidden';
+    document.getElementById('blackout').style.display = 'block';
+    document.getElementById('about-box').style.display = 'block';
+    document.getElementById('blackout').onclick = () => {
+        document.body.style.overflow = 'auto';
+        document.getElementById('blackout').style.display = 'none';
+        document.getElementById('about-box').style.display = 'none';
+    }
+}
